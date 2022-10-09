7bcdd8a6b5f7fa5ae62fee0ec9110920bf694705
moqiq <moqiqin@live.com>
2022-10-09 23:24:43 +0800

    update plot

--- analyser.py
+++ analyser.py
@@ -10,7 +10,6 @@ import base64
 import tabulate
 
 
-
 class SangerBaseCall:
     def __init__(self, sanger_file):
         self.sanger_data = SeqIO.read(sanger_file, "abi")
@@ -39,6 +38,9 @@ class SangerBaseCall:
                 self.labels[i] = self.location_base_annotation[i]
             else:
                 self.labels[i] = ""
+
+        # 整体的噪音
+        self.noise = {"G": [], "A": [], "T": [], "C": []}
 
     def locTartet(self, target_seq, max_mismatch=2):
         # Find the match site and location
@@ -117,12 +119,19 @@ class SangerBaseCall:
 
         fig, ax = plt.subplots()
 
+        for base_i in range(len(labels)):  # 在图上标注碱基
+            base = labels[base_i]
+            if base != "":
+                plt.text(base_i - 1, -100, base)
+
         plt.tick_params(axis="x",
                         length=0,  # tick长度
                         width=0,  # tick的宽度
-                        labelsize=10,  # 刻度值大小
+                        labelsize=0,  # 刻度值大小
+                        bottom=False
+
                         )
-        plt.xticks(ticks=range(plot_end - plot_start), labels=labels, label="sequence")
+        # plt.xticks(ticks=range(plot_end - plot_start), labels=labels, label="sequence")
         plt.xlabel("Sequence")
         plt.ylabel('Sanger Signal Intensity')
         G = ax.plot(trace_G, color=colors[0], label='G')
@@ -135,20 +144,21 @@ class SangerBaseCall:
 
         if sg_plot_start < sg_plot_end:
             sg_plot_end = self.annotation[target_location[2] - 1][1]
-            SG = ax.plot([sg_plot_start - plot_start, sg_plot_end - plot_start], [-100, -100], color="brown",
+            SG = ax.plot([sg_plot_start - plot_start, sg_plot_end - plot_start], [-150, -150], color="brown",
                          label='sgRNA', marker=">")
 
 
 
         else:
             sg_plot_start = self.annotation[target_location[1]][1]
-            SG = ax.plot([sg_plot_end - plot_start, sg_plot_start - plot_start - 9], [-100, -100], color="purple",
+            SG = ax.plot([sg_plot_end - plot_start, sg_plot_start - plot_start - 9], [-150, -150], color="purple",
                          label='sgRNA', marker="<")
 
         plt.legend()
         return plt
 
     def getTargetData(self, target_seq, annalyse_window=10):
+        """获取靶位点上各个碱基的信号占比，以及靶位点外的噪音平均值"""
         # plt.rcParams['figure.figsize'] = (15, 4)
         # colors = ("black", "green", "red", "blue")  # GATC
         target_location = self.locTartet(target_seq)
@@ -173,6 +183,59 @@ class SangerBaseCall:
         plot_start = self.annotation[base_start][1]
         plot_end = self.annotation[base_end][1]
 
+        # 噪声统计区间
+        noise_start_1 = 100
+        noise_end_1 = base_start - 20
+        noise_start_2 = base_end + 20
+
+        try:
+            noise_end_2 = 700
+        except Exception as e:
+            print("sanger file is too short!")
+            noise_end_2 = base_end + 120
+
+        self.noise = {"G": [], "A": [], "T": [], "C": []}  # 用来记录噪音，后续计算平均值和方差
+        print(noise_start_1, noise_end_1)
+        for i in range(noise_start_1, noise_end_1):
+            if i in self.location_base_annotation:
+                peak = i
+                peak_base = self.location_base_annotation[peak]
+                peak_base_num = list(self.location_base_annotation.keys()).index(peak)
+                peak_left_border = peak - 3
+                peak_right_border = peak + 3
+
+                signal = {}
+                signal["G"] = self.trace["G"][peak_left_border:peak_right_border]
+                signal["A"] = self.trace["A"][peak_left_border:peak_right_border]
+                signal["T"] = self.trace["T"][peak_left_border:peak_right_border]
+                signal["C"] = self.trace["C"][peak_left_border:peak_right_border]
+
+                for base in ["G", "A", "T", "C"]:
+                    # print(base+peak_base)
+                    if base != peak_base:
+                        # print(signal[base])
+                        self.noise[base].append(signal[base])
+
+        for i in range(noise_start_2, noise_end_2):
+            if i in self.location_base_annotation:
+                peak = i
+                peak_base = self.location_base_annotation[peak]
+                peak_base_num = list(self.location_base_annotation.keys()).index(peak)
+                peak_left_border = peak - 3
+                peak_right_border = peak + 3
+
+                signal = {}
+                signal["G"] = self.trace["G"][peak_left_border:peak_right_border]
+                signal["A"] = self.trace["A"][peak_left_border:peak_right_border]
+                signal["T"] = self.trace["T"][peak_left_border:peak_right_border]
+                signal["C"] = self.trace["C"][peak_left_border:peak_right_border]
+
+                for base in ["G", "A", "T", "C"]:
+                    # print(base+peak_base)
+                    if base != peak_base:
+                        # print(signal[base])
+                        self.noise[base].append(signal[base])
+
         result = {}
         # fig, ax = plt.subplots()
         sheet = pd.DataFrame(index=["G", "A", "T", "C", "sequence", "guide"])
@@ -185,6 +248,10 @@ class SangerBaseCall:
                 # print(peak_trace)eak_val = peak_trace[peak]
 
                 """
+                注释掉这一段。这段代码用于查找整个信号的汉宁窗口，然后再以该窗口计算信号、噪音。
+                但是，窗口边界可能与其它碱基的窗口重叠，从而导致噪音误判。
+
+                只采用信号峰值附近的应该就够了。
                 peak_left_data = np.array(peak_trace[peak-10:peak][::-1])
                 peak_right_data = np.array(peak_trace[peak:peak+10])
 
@@ -246,18 +313,20 @@ class SangerBaseCall:
 
         return sheet
 
-    def plotTargetHeatmap(self, data_sheet, base_of_interest=""):
+    def plotTargetHeatmap(self, data_sheet, base_of_interest="", color_map="YlGn"):
         from matplotlib import pyplot as plt
         plt.cla()
         plt.close("all")
         sequence = data_sheet.loc["sequence"]
         columns = ["G", "A", "T", "C", "Guide"]
         sg_data = []
+
+        # 给sgRNA的位置也赋值，从而带上颜色
         for base in data_sheet.loc["guide"]:
             if base == "":
-                sg_data.append(0)
+                sg_data.append(-15)
             else:
-                sg_data.append(70)
+                sg_data.append(-50)
 
         data = np.array([data_sheet.loc["G"],
                          data_sheet.loc["A"],
@@ -266,8 +335,9 @@ class SangerBaseCall:
                          sg_data]).astype("float")
 
         fig, ax = plt.subplots(figsize=(20, 6))
-        ax.set_title("Each base (ACGT) signal intensity at each position along " + "".join(list(data_sheet.loc["guide"])))
-        im = ax.imshow(data, cmap="YlGn")
+        ax.set_title(
+            "Each base (ACGT) signal intensity at each position along " + "".join(list(data_sheet.loc["guide"])))
+        im = ax.imshow(data, cmap=color_map)
 
         ax.set_xticks(np.arange(len(sequence)), labels=sequence)
         ax.set_yticks(np.arange(len(columns)), labels=columns)
@@ -297,7 +367,6 @@ class SangerBaseCall:
 
         return plt
 
-
     def plt2md(self, plt):
         buffer = BytesIO()
         plt.savefig(buffer, format='png')
@@ -307,12 +376,12 @@ class SangerBaseCall:
         md = """![](data:image/png;base64,""" + pic_base64 + """)"""
         return md
 
-    def generateReport(self, target_seq, report_name, save_path ,plot_window=5, base_of_interest=""):
+    def generateReport(self, target_seq, report_name, save_path, plot_window=5, base_of_interest=""):
         sanger_plot = self.plotTarget(target_seq=target_seq, plot_window=plot_window)
         sanger_plot_md = self.plt2md(sanger_plot)
 
         perc_around_guide_sheet = self.getTargetData(target_seq=target_seq, annalyse_window=plot_window)
-        heatmap_plot = self.plotTargetHeatmap(perc_around_guide_sheet,base_of_interest)
+        heatmap_plot = self.plotTargetHeatmap(perc_around_guide_sheet, base_of_interest)
         heatmap_plot_md = self.plt2md(heatmap_plot)
 
         title = "# BaseEdit Report of " + report_name + "\n"
@@ -324,7 +393,6 @@ class SangerBaseCall:
         sub_title_1 = "\n\n## Sanger sequencing result around " + target_seq + "\n\n"
         Fig1_annotation = "\n\nThis figure shows the Sanger sequencing signal quality."
 
-
         sub_title_2 = "\n\n## Base call result in each position around " + target_seq + "\n"
         Fig2_annotation = """\n\nThis figure shows the possibilities of each base at each sequencing position. 
                                 Guide position at the botton line shows the relative position to your guide RNA.\n"""
@@ -332,13 +400,12 @@ class SangerBaseCall:
             Fig2_annotation = Fig2_annotation + "\n\nThe red rectangle is the base you want to convert to."
             description = description + "\n\nThis sample is supposed to be converted to " + base_of_interest
 
-
-
         sub_title_3 = "\n\n## Base call table in each position around " + target_seq + "\n"
         sheet = perc_around_guide_sheet.to_markdown()
         sheet_annotation = """\n\n This sheet shows the possibilities of each base at each sequencing position.\n\nIt's the same as the heatmap above.
                                 \n\nYou can copy the entire sheet and pasted it to Excel for further analysis.\n\n"""
-        content = [title, description, sep, sub_title_1, sanger_plot_md,Fig1_annotation, sep,sub_title_2, heatmap_plot_md,Fig2_annotation, sep, sub_title_3, sheet_annotation, sheet]
+        content = [title, description, sep, sub_title_1, sanger_plot_md, Fig1_annotation, sep, sub_title_2,
+                   heatmap_plot_md, Fig2_annotation, sep, sub_title_3, sheet_annotation, sheet]
 
         html_content = markdown.markdown("".join(content), extensions=["tables"])
         with open(save_path, "w") as f:
