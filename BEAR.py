6178697ac3bbbde2bfe851667bc220b8ae1cc3e6
moqiq <moqiqin@live.com>
2022-11-19 00:28:24 +0800

    比对精确度

--- BEAR.py
+++ BEAR.py
@@ -37,7 +37,7 @@ class MyMainWin(QMainWindow, Ui_MainWindow):
         super(MyMainWin, self).__init__(parent)
 
         self.setupUi(self)
-        self.version = "1.2.0"
+        self.version = "1.2.1"
         output = "BaseEdit-Analyser" + "\n\t\t\t" + "——Written by M.Q. at ShanghaiTech University"
         self.label_lyric.setText(output)
         self.selected_rows = []
